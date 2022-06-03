aebbca437e96177366925f9656db2ae85ca0a416
Annas-Ahmed <annasahmed1609@gmail.com>
2022-06-04 02:13:52 +0500

    tast7 complete

---
+++ app.js
@@ -0,0 +1,74 @@
+// question 43-48
+
+// question3
+
+// deleteButton=document.getElementsByClassName("deleteButton")
+// function deleteRow(e){
+//     node=deleteButton[e].parentNode.parentNode
+//     node.innerHTML=""
+// }
+
+// question4
+// bulbImage=document.getElementById("bulb")
+// function bulbchange(parameter){
+//     if(parameter=='on'){
+//         bulbImage.src="bulbOnn.png"
+//     }
+//     if(parameter=='off'){
+//         bulbImage.src="bulbOff.png"
+//     }
+// }
+
+// question5
+// count=document.getElementById("counter")
+// function decreaseCounter(e){
+//     decrease=document.getElementById(buttonMinus)
+//     count.textContent-=1
+// }
+
+// function increaseCounter(e){    
+//     increase=document.getElementById(buttonPlus)
+//     count.textContent=parseInt(count.textContent)+1
+// }
+
+// Chapter 49-52
+
+// question1
+// function getData(){
+//     name=document.getElementById("nameInput")
+//     contact=document.getElementById("contactInput")
+//     email=document.getElementById("emailInput")
+//     password=document.getElementById("passwordInput")
+
+//     console.log("name = "+name.value)
+//     console.log("contact = "+contact.value)
+//     console.log("email = "+email.value)
+//     console.log("password = "+password.value)
+// }
+
+// question2
+
+// function seeMore(e){
+//     e.parentNode.textContent+='lorem mkcmsl lmcslcm olmlcsmck kmclasmcasl kmclasmcl mclsmc'
+// }
+
+// question3
+
+// function getData(){
+//     name2=document.getElementById("nameInput")
+//     contact=document.getElementById("contactInput")
+//     email=document.getElementById("emailInput")
+//     password=document.getElementById("passwordInput")
+//     table=document.getElementById("table")
+//     row=table.insertRow()
+//     name1=row.insertCell(0)
+//     name1.textContent=name2.value
+//     console.log(name1.textContent)
+//     console.log(name2.value)
+//     contact1=row.insertCell(1)
+//     contact1.textContent=contact.value
+//     email1=row.insertCell(2)
+//     email1.textContent=email.value
+//     password1=row.insertCell(3)
+//     password1.textContent=password.value
+// }
